f97a30af0456d34ef07a784cd7c4bb97568b43de
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-05-02 06:11:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,6 @@ resource "aws_api_gateway_rest_api" "my_api_gateway" {
   name = "my-open-api-gateway"
   #body = data.openapi3_spec.my_api.json
   #body = file("${path.module}/openapi3_spec.my_api.yaml")
-  body = data.template_file.my_api.rendered
+ # body = data.template_file.my_api.rendered
+  body = data.template_file.my_api
 }
