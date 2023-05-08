3a15fd2fb7a59cbbd1b0e59ea25bdd89aa1edb7f
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-05-08 10:18:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,8 @@ resource "aws_api_gateway_rest_api" "my_api_gateway" {
   body = data.template_file.my_api.rendered
   
 }
+
+vars = {
+    aws_region              = var.AWS_REGION
+    account_id              = var.account_map
+  }
