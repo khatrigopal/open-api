1ca3c3f2a3a358dcb993046f8f4ca145395a9efc
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-05-12 15:56:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "aws_api_gateway_rest_api" "my_api" {
 resource "aws_lambda_permission" "allow_open_api" {
   statement_id = "AllowAPIGatewayInvokenew"
   action = "lambda.InvokeFunction"
-  function_name
+  function_name = "test-lambda"
   principal = "apigateway.amazonaws.com"
   source_arn = "arn:aws:execute-api:us-west1-558940753150-${aws_api_gateway_rest_api.my_api}/*/*/*"
